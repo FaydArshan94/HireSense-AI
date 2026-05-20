500f3025f5ae0f7bf397605060935fbce59667bb
Fayd Arshan94 <arshanw94@gmail.com>
2026-05-20 18:37:21 +0530

    feat: implement configured axios instance with request and response interceptors for authentication

--- frontend/src/utils/axios/axios.jsx
+++ frontend/src/utils/axios/axios.jsx
@@ -2,7 +2,7 @@ import { useAuthStore } from "../../store/authStore";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://hiresense-ai-bcxp.onrender.com/api",
   withCredentials: true,
 });
 
